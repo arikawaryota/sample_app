e52a6fbf3634fc03936fe2f383b2c17e184ccfb0
arikawaryota <ryouta.paka09@gmail.com>
2024-10-25 07:58:08 +0000

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
+    # reder ....
+    # => app/views/static_pages/home.html.erb
   end
 
   def help
+    # => app/views/static_pages/help/html.erb
+  end
+
+  def about
+    # => app/views/static_pages/about.html.erb
   end
 end
